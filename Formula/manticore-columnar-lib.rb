f99d3eb1d04cbfa7d849d79f09def1208f60ef96
Don Hardman <don.hardman@muvon.io>
2023-02-03 08:26:18 +0000

    Update dev versions of buddy and backup

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -7,12 +7,6 @@ class ManticoreColumnarLib < Formula
   version_scheme 1
   head "https://github.com/manticoresoftware/columnar.git"
 
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-1.16.1-2023012002-d0da462"
-    sha256 cellar: :any_skip_relocation, monterey: "b0fc85d8e763c8c7ab993032a7e2aaff9cb1a037700ca86c62977e69fcd03f5a"
-    sha256 cellar: :any_skip_relocation, big_sur:  "9262e53bdc065f752484b1060bb94f010f73d23aa39c7a1fb589e613a71736b2"
-  end
-
   depends_on "boost" => :build
   depends_on "cmake" => :build
 
