3c5bfb325d1404fef6d1910ca996614e0aab8286
Sergey Nikolaev <prostuda@academ.org>
2023-03-05 13:41:24 +0700

    MCL 2.0.2

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -1,18 +1,11 @@
 class ManticoreColumnarLib < Formula
   desc "Manticore Columnar Library"
   homepage "https://github.com/manticoresoftware/columnar/"
-  url "https://github.com/manticoresoftware/columnar.git", branch: "2.0.0", revision: "a7c703d90351b696a58ab6573059fac7b5aa8283"
-  version "2.0.0-2023020711-a7c703d"
+  url "https://github.com/manticoresoftware/columnar.git", branch: "columnar-2.0.2", revision: "b0dd7ad923d7db2646705729d822027f0e248f11"
+  version "2.0.2-2023030309-b0dd7ad"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/columnar.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-2.0.0-2023020711-a7c703d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "200747b6f211c4a0c054c67a626b0f89852eaa6b534c49c1665994722320cb7a"
-    sha256 cellar: :any_skip_relocation, monterey:      "d99a53878426eca7d535c96909d716badfd1b68f3674c3b4fb98f82dccee7b98"
-    sha256 cellar: :any_skip_relocation, big_sur:       "94f84f26a7b738d1327a3c5e739d9828bd842c2d175a0f9b6b4acfcb4c4d38a5"
-  end
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
