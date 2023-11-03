88dc4d2c3385a061443b52c8bc48901e8df6050c
Sergey Nikolaev <prostuda@academ.org>
2023-11-03 21:46:14 +0700

    test

--- Formula/manticore-extra.rb
+++ Formula/manticore-extra.rb
@@ -1,5 +1,5 @@
 require "digest"
-require 'hardware'
+#require 'hardware'
 
 class ManticoreExtra < Formula
   desc "Manticore meta package to install manticore-executor and manticore-columnar-lib dependencies"
@@ -10,12 +10,12 @@ class ManticoreExtra < Formula
   puts "sha256 " + Digest::SHA256.file(File.expand_path(__FILE__)).hexdigest
 
   # we take version of "executor"
-  arch = Hardware::CPU.arch
-  if arch.to_s == "x86_64" || arch.to_s == "amd64"
+#  arch = Hardware::CPU.arch
+#  if arch.to_s == "x86_64" || arch.to_s == "amd64"
     version "0.7.8-230822-810d7d3"
-  else
-    version "0.7.6-230804-8f5cfa5"
-  end
+#  else
+#    version "0.7.6-230804-8f5cfa5"
+#  end
 
   depends_on "sanikolaev/tap/manticore-columnar-lib"
   depends_on "sanikolaev/tap/manticore-executor"
