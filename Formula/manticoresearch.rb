40da6f190716d9b09553cad5c76dfab0cb7ba829
Sergey Nikolaev <prostuda@academ.org>
2025-02-28 22:53:52 +0700

    fixed version in manticoresearch.rb

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -7,7 +7,7 @@ class Manticoresearch < Formula
 
   arch = Hardware::CPU.arch
   url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-7.0.0-25013002-92c650401-osx11.6-#{arch}-main.tar.gz"
-  version "7.0.0-25013002-92c650401"
+  version "7.4.6-25022808-b2ff82920"
 
   if arch.to_s == "x86_64" || arch.to_s == "amd64"
     sha256 "9ff2e1b64beb276e2cdd7ec3be787df74116a0ab8f197187e37eb8db5940f367"
