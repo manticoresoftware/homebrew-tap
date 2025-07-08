4dbea388fd728aefbc14fa7de5a239928c284cff
Sergey Nikolaev <prostuda@academ.org>
2025-07-08 22:24:54 +0700

    fix to prev

--- Formula/manticore-executor.rb
+++ Formula/manticore-executor.rb
@@ -10,11 +10,11 @@ class ManticoreExecutor < Formula
 
   if arch.to_s == "x86_64" || arch.to_s == "amd64"
     version "1.3.5+250708-6c4be4c"
-    url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-executor_#{version}-macos-x86_64.tar.gz"
+    url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-executor-#{version}-macos-x86_64.tar.gz"
     sha256 "dd152d87b5c48e91b00134397d4aadf8b4a76c85de99597e3ef9738408b3bd87"
   else
     version "1.3.5+250708-6c4be4c"
-    url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-executor_#{version}-macos-arm64.tar.gz"
+    url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-executor-#{version}-macos-arm64.tar.gz"
     sha256 "00676b4c5e64a5ae2cdb4c5c002d1868ee409f7f48b0bce32bea7f4addbc10b8"
   end
 
