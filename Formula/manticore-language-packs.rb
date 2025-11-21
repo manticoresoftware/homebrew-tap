8a932fe2b9cb1f965c4478ecc2948271b5694a57
Sergey Nikolaev <prostuda@academ.org>
2025-11-21 21:26:29 +0700

    Enhance ManticoreLanguagePacks formula: streamline installation process for morphology and jieba dictionary files

--- Formula/manticore-language-packs.rb
+++ Formula/manticore-language-packs.rb
@@ -5,11 +5,31 @@ class ManticoreLanguagePacks < Formula
   version "0.9.9"
 
   def install
-    (share/"manticore").mkpath
+    manticore_share = share/"manticore"
+    manticore_share.mkpath
 
-    langs = %w[de en ru]
-    langs.each do |lang|
-      system "curl -sSL https://repo.manticoresearch.com/repository/morphology/#{lang}.pak.tgz | tar xvzf - -C #{share/"manticore"}"
+    # Download and unpack morphology .pak files
+    %w[de en ru].each do |lang|
+      archive = "#{lang}.pak.tgz"
+      system "curl", "-sSL",
+             "https://repo.manticoresearch.com/repository/morphology/#{lang}.pak.tgz",
+             "-o", archive
+      system "tar", "xzf", archive, "-C", manticore_share.to_s
     end
+
+    # Directory for jieba dict files (sibling to the .pak files)
+    jieba_dir = manticore_share/"jieba"
+    jieba_dir.mkpath
+
+    # Download jieba dict files
+    %w[hmm_model idf stop_words user.dict].each do |name|
+      system "curl", "-sSL",
+             "https://raw.githubusercontent.com/manticoresoftware/cppjieba/master/dict/#{name}.utf8",
+             "-o", (jieba_dir/"#{name}.utf8")
+    end
+
+    system "curl", "-sSL",
+           "https://raw.githubusercontent.com/manticoresoftware/jieba/refs/heads/master/extra_dict/dict.txt.big",
+           "-o", (jieba_dir/"jieba.dict.utf8")
   end
 end
