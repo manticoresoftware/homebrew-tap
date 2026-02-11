ed26fe1c8f789ece59b8291e488b6af36a61dc1b
Sergey Nikolaev <prostuda@academ.org>
2026-02-11 20:26:31 +0700

    MCL 10.2.2

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -6,13 +6,13 @@ class ManticoreColumnarLib < Formula
   license "Apache-2.0"
 
   arch = Hardware::CPU.arch
-  version "10.2.0+26020513-9a5dd2f5"
+  version "10.2.2+26021001-a705ab6c"
   url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-columnar-lib-#{version}-osx11.6-#{arch}.tar.gz"
 
   if arch.to_s == "x86_64" || arch.to_s == "amd64"
-    sha256 "6caa2ba40c2e7c344583fa794ff6ca8dc3a4fd443dca85ce53be08db47a562bf"
+    sha256 "dfde31b6461e529227d4b48e43cbf5c80d5d51157930fec500306f6df1bee6ed"
   else
-    sha256 "af868b3f5c1ece2b0ab2fc0b9005563d75469291b88b1170e97a36d5748af881"
+    sha256 "6f9d02efeb12e993aadf44ea62fb048ab5612ead8341eac0d266896a75cc8a80"
   end
 
   def install
